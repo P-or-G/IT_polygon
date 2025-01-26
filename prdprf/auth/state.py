0b2b22a34d20560064ec1952c6ff1d312fa14318
giant <giantfedor@gmail.com>
2025-01-26 21:37:37 +0300

    Переписал класс localuserauth

--- prdprf/auth/state.py
+++ prdprf/auth/state.py
@@ -6,79 +6,11 @@ import reflex_local_auth
 
 import sqlmodel
 from reflex.event import EventSpec
-from reflex_local_auth import LocalUser
+from reflex_local_auth.auth_session import LocalAuthSession
 from sqlmodel import select
 
 from prdprf.models import UserInfo
 from prdprf.navigation.routes import LOGIN_ROUTE, REGISTER_ROUTE
-
-
-class SessionState(reflex_local_auth.LocalAuthState):
-    """
-    Тебе не нужно знать, что тут происходит
-    |
-    |
-    |
-    Тут обрабатывается состояние сессии, а ещё отсюда получаем все данные по пользователю из UserInfo
-    А ещё обрабатываем состояние пользователя (вошёл или нет), здесь же делаем некоторый logout
-    Вся реальная муть с обработкой состояний сессии в reflex_local_auth.LocalAuthState
-    !!!МОЖЕТ БЫТЬ НУЖНО БУДЕТ ДОБАВИТЬ ВОЗВРАЩЕНИЕ ДРУГИХ СТОЛБИКОВ!!!
-    """
-    @rx.var(cache=True)
-    def my_userinfo_id(self) -> str | None:
-        return self.authenticated_user_info.id
-
-    @rx.var(cache=True)
-    def my_user_id(self) -> str | None:
-        """
-        Если id больше или 0, оно скажет id, а меньше нуля тебе зачем?
-        """
-        if self.authenticated_user.id < 0:
-            return None
-        return self.authenticated_user.id
-    
-    @rx.var(cache=True)
-    def authenticated_username(self) -> str | None:
-        """
-        Мы возвращаем только имя, если не можем (сессия нужна), то None
-        """
-        if self.authenticated_user.id < 0:
-            return None
-        return self.authenticated_user.username
-
-    @rx.var(cache=True)
-    def authenticated_user_info(self) -> UserInfo | None:
-        """
-        Мы возвращаем ВСЁ, что есть в UserInfo (дада, весь класс), если не можем (сессия нужна), то None
-        """
-        if self.authenticated_user.id < 0:
-            return None
-        with rx.session() as session:
-            result = session.exec(
-                sqlmodel.select(UserInfo).where(
-                    UserInfo.user_id == self.authenticated_user.id
-                ),
-            ).one_or_none()
-            if result is None:
-                return None
-            return result
-    
-    def on_load(self):
-        """
-        Проверяет зарегистрирован ли пользователь, если нет, то возвращает ивент редиректа на /
-        Кроме случаев, когда мы на /login
-        """
-        if not self.is_authenticated:
-            return reflex_local_auth.LoginState.redir
-        print(self.is_authenticated)
-        print(self.authenticated_user_info)
-
-    def perform_logout(self) -> EventSpec:
-        """
-        Обычный do_logout, но мы делаем редирект в итоге
-        """
-        self.do_logout()
-        return rx.redirect("/")
 
 
 def redir():
@@ -230,3 +162,131 @@ class MyLoginState(reflex_local_auth.LoginState):
         self.is_hydrated = True
         self.is_authenticated = True
         return self.redir()
+
+
+AUTH_TOKEN_LOCAL_STORAGE_KEY = "_auth_token"
+DEFAULT_AUTH_SESSION_EXPIRATION_DELTA = datetime.timedelta(days=7)
+DEFAULT_AUTH_REFRESH_DELTA = datetime.timedelta(minutes=10)
+
+
+class MyLocalAuthState(rx.State):
+    auth_token: str = rx.LocalStorage(name=AUTH_TOKEN_LOCAL_STORAGE_KEY)
+
+    @rx.var(cache=True, interval=DEFAULT_AUTH_REFRESH_DELTA)
+    def authenticated_user(self) -> UserInfo:
+
+        with rx.session() as session:
+            result = session.exec(
+                select(UserInfo, LocalAuthSession).where(
+                    LocalAuthSession.session_id == self.auth_token,
+                    LocalAuthSession.expiration
+                    >= datetime.datetime.now(datetime.timezone.utc),
+                    UserInfo.id == LocalAuthSession.user_id,
+                ),
+            ).first()
+            if result:
+                user, session = result
+                return user
+        return UserInfo(id=-1)  # type: ignore
+
+    @rx.var(cache=True, interval=DEFAULT_AUTH_REFRESH_DELTA)
+    def is_authenticated(self) -> bool:
+        return self.authenticated_user.id >= 0
+
+    def do_logout(self) -> None:
+        with rx.session() as session:
+            for auth_session in session.exec(
+                select(LocalAuthSession).where(LocalAuthSession.session_id == self.auth_token)
+            ).all():
+                session.delete(auth_session)
+            session.commit()
+        self.auth_token = self.auth_token
+
+    def _login(
+        self,
+        user_id: int,
+        expiration_delta: datetime.timedelta = DEFAULT_AUTH_SESSION_EXPIRATION_DELTA,
+    ) -> None:
+        self.do_logout()
+        if user_id < 0:
+            return
+        self.auth_token = self.auth_token or self.router.session.client_token
+        with rx.session() as session:
+            session.add(
+                LocalAuthSession(  # type: ignore
+                    user_id=user_id,
+                    session_id=self.auth_token,
+                    expiration=datetime.datetime.now(datetime.timezone.utc)
+                    + expiration_delta,
+                )
+            )
+            session.commit()
+
+
+class SessionState(MyLocalAuthState):
+    """
+    Тебе не нужно знать, что тут происходит
+    |
+    |
+    |
+    Тут обрабатывается состояние сессии, а ещё отсюда получаем все данные по пользователю из UserInfo
+    А ещё обрабатываем состояние пользователя (вошёл или нет), здесь же делаем некоторый logout
+    Вся реальная муть с обработкой состояний сессии в reflex_local_auth.LocalAuthState
+    !!!МОЖЕТ БЫТЬ НУЖНО БУДЕТ ДОБАВИТЬ ВОЗВРАЩЕНИЕ ДРУГИХ СТОЛБИКОВ!!!
+    """
+
+    @rx.var(cache=True)
+    def my_userinfo_id(self) -> str | None:
+        return self.authenticated_user_info.id
+
+    @rx.var(cache=True)
+    def my_user_id(self) -> str | None:
+        """
+        Если id больше или 0, оно скажет id, а меньше нуля тебе зачем?
+        """
+        if self.authenticated_user.id < 0:
+            return None
+        return self.authenticated_user.id
+
+    @rx.var(cache=True)
+    def authenticated_username(self) -> str | None:
+        """
+        Мы возвращаем только имя, если не можем, то None
+        """
+        if self.authenticated_user.id < 0:
+            return None
+        return self.authenticated_user.username
+
+    @rx.var(cache=True)
+    def authenticated_user_info(self) -> UserInfo | None:
+        """
+        Мы возвращаем ВСЁ, что есть в UserInfo (дада, весь класс), если не можем, то None
+        """
+        if self.authenticated_user.id < 0:
+            return None
+        with rx.session() as session:
+            result = session.exec(
+                sqlmodel.select(UserInfo).where(
+                    UserInfo.id == self.authenticated_user.id
+                ),
+            ).one_or_none()
+            if result is None:
+                return None
+            return result
+
+    def on_load(self):
+        """
+        Проверяет зарегистрирован ли пользователь, если нет, то возвращает ивент редиректа на /
+        Кроме случаев, когда мы на /login
+        """
+        if not self.is_authenticated:
+            return reflex_local_auth.LoginState.redir
+        print(self.is_authenticated)
+        print(self.authenticated_user_info)
+
+    def perform_logout(self) -> EventSpec:
+        """
+        Обычный do_logout, но мы делаем редирект в итоге
+        """
+        self.do_logout()
+        return rx.redirect("/")
