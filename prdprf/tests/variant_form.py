df9a0f037e94bd762de688fbb9e9f942f650a7a3
giant <giantfedor@gmail.com>
2025-02-10 22:39:10 +0300

    ...

--- prdprf/tests/variant_form.py
+++ prdprf/tests/variant_form.py
@@ -2,10 +2,7 @@ import reflex as rx
 
 import reflex_local_auth
 
-from prdprf.auth.state import SessionState
 from prdprf.models import Question
-from prdprf.tests.questions_form import add_question_form
-from prdprf.tests.subj_state import SubjectListState, question_list_item
 from prdprf.tests.variant_state import VariantState
 from prdprf.ui.base import base_page
 
